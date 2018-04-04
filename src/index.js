e357568cbdf99c5bce18748ad2ebb87c06b31f65
Zahran Kabir <zahran@Zahrans-MacBook-Pro.local>
2018-04-04 11:46:46 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import ControlledInput from './components/ControlledInput'
 ReactDOM.render(
-  <div>Mount Components Here</div>,
+  <ControlledInput />,
   document.getElementById('root')
 );
